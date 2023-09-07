9aa8c2cdfa0215376e990ae20a0f44e588693555
Bramus <bramus@bram.us>
2023-09-07 13:40:47 +0200

    ✨ Background Script: Add support for redirecting status updates

--- src/background.js
+++ src/background.js
@@ -1,50 +1,81 @@
 const go = () => {
+	let MY_MASTO_LOCAL_DOMAIN = null;
+	let MY_MASTO_WEB_DOMAIN = null;
+
+	const tryAndGetUserNameFromProfilePage = () => {
+		/* Profile with a moved banner (e.g. https://mastodon.social/@bramus): follow that link */
+		const userNewProfile = document.querySelector('.moved-account-banner .button')?.getAttribute('href');
+		if (userNewProfile) {
+			return userNewProfile.substring(2);
+		}
+
+		/* Profile page, e.g. https://fediverse.zachleat.com/@zachleat and https://front-end.social/@mia */
+		const userFromProfilePage = document.querySelector('.account__header .account__header__tabs__name small')?.innerText.substring(1);
+		if (userFromProfilePage) {
+			return userFromProfilePage;
+		}
+
+		// Not a profile page or some markup that is preventing things from happening
+		return null;
+	};
+
+	const getProfileRedirectUrl = () => {
+		let user = tryAndGetUserNameFromProfilePage();
+
+		// Found user ~> Redirect to profile on own host
+		if (user) {
+			/* Trim off @domain suffix in case it matches with MY_MASTO_LOCAL_DOMAIN. This due to https://github.com/mastodon/mastodon/issues/21469 */
+			if (user.endsWith(`@${MY_MASTO_LOCAL_DOMAIN}`)) {
+				user = user.substring(0, user.length - `@${MY_MASTO_LOCAL_DOMAIN}`.length);
+			}
+
+			return `https://${MY_MASTO_WEB_DOMAIN}/@${user}`;
+		}
+
+		return null;
+	};
+
+	const getPostRedirectUrl = () => {
+		// We resort to URL sniffing here … sorry
+		const urlPathParts = window.location.pathname.substr('1').split('/');
+
+		// The path must be something like /@user/12345
+		if (urlPathParts.length != 2) return null;
+		if (!Number.isInteger(parseInt(urlPathParts[1]))) return null;
+
+		// It is quite safe to assume this was a Mastodon post detail
+		return `https://${MY_MASTO_WEB_DOMAIN}/authorize_interaction?uri=${encodeURIComponent(window.location.href)}`;
+	};
+
 	chrome.storage.sync.get(
 		{
 			local_domain: '',
 			web_domain: '',
 		},
 		function (items) {
-			const MY_MASTO_LOCAL_DOMAIN = items.local_domain;
-			const MY_MASTO_WEB_DOMAIN = items.web_domain || MY_MASTO_LOCAL_DOMAIN;
+			MY_MASTO_LOCAL_DOMAIN = items.local_domain;
+			MY_MASTO_WEB_DOMAIN = items.web_domain || MY_MASTO_LOCAL_DOMAIN;
 
 			if (!MY_MASTO_LOCAL_DOMAIN) {
 				alert('Please go to options and set your MY_MASTO_LOCAL_DOMAIN first');
 				return;
 			}
 
-			const tryAndGetUserName = () => {
-				/* Profile with a moved banner (e.g. https://mastodon.social/@bramus): follow that link */
-				const userNewProfile = document.querySelector('.moved-account-banner .button')?.getAttribute('href');
-				if (userNewProfile) {
-					return userNewProfile.substring(2);
-				}
+			// Don’t do anything if already on the mastodon domain
+			if (window.location.host === MY_MASTO_LOCAL_DOMAIN) return null;
 
-				/* Profile page, e.g. https://fediverse.zachleat.com/@zachleat and https://front-end.social/@mia */
-				/* First try the username meta tag. However, sometimes Mastodon forgets to inject it, so we fall back to the username shown in the profile header */
-				const userFromProfilePage = document.querySelector('meta[property="profile:username"]')?.getAttribute('content') || document.querySelector('.account__header .account__header__tabs__name small')?.innerText.substring(1);
-				if (userFromProfilePage) {
-					/* Don’t return if already watching on own LOCAL_DOMAIN instance */
-					if (window.location.host === MY_MASTO_LOCAL_DOMAIN) return null;
-					return userFromProfilePage;
-				}
+			const extractors = [getProfileRedirectUrl, getPostRedirectUrl];
 
-				/* Message detail, e.g. https://front-end.social/@mia/109348973362020954 and https://bell.bz/@andy/109392510558650993 and https://bell.bz/@andy/109392510558650993 */
-				const userFromDetailPage = document.querySelector('.detailed-status .display-name__account')?.innerText;
-				if (userFromDetailPage) return userFromDetailPage.substring(1);
+			let redirectUrl = null;
+			do {
+				redirectUrl = extractors.shift()();
+			} while (redirectUrl == null && extractors.length);
 
-				return null;
-			};
-
-			let user = tryAndGetUserName();
-			if (!user) return;
-
-			/* Trim off @domain suffix in case it matches with MY_MASTO_LOCAL_DOMAIN. This due to https://github.com/mastodon/mastodon/issues/21469 */
-			if (user.endsWith(`@${MY_MASTO_LOCAL_DOMAIN}`)) {
-				user = user.substring(0, user.length - `@${MY_MASTO_LOCAL_DOMAIN}`.length);
+			if (redirectUrl) {
+				window.location.href = redirectUrl;
+			} else {
+				alert('No Mastodon profile or Mastodon post detected. If this is incorrect, please file a bug.');
 			}
-
-			window.location.href = `https://${MY_MASTO_WEB_DOMAIN}/@${user}`;
 		}
 	);
 };
